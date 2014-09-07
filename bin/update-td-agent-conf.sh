d43ee34da7443c5dad094749b7fcaec78c22f743
oppara <oppara@oppara.tv>
2014-09-07 20:16:33 +0900

    Fix startup script permission of td-agent

--- bin/update-td-agent-conf.sh
+++ bin/update-td-agent-conf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo mv /etc/td-agent/td-agent.conf /etc/td-agent/td-agent.conf.backup
 sudo cp /vagrant/conf/td-agent.conf /etc/td-agent/td-agent.conf
-/etc/init.d/td-agent restart
+sudo service td-agent restart
